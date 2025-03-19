37ce64c8c7116f787a12b4cb692864315e71e60c
Haridath-Bodapati <101719406+Haridath-Bodapati@users.noreply.github.com>
2025-03-19 19:53:10 +0000

    Add files via upload

---
+++ core_basics/src/com/inet/training/AccessDefaultSpecifier.java
@@ -0,0 +1,11 @@
+package com.inet.training;
+import com.inet.training.defaultpackage.*;
+
+public class AccessDefaultSpecifier {
+	
+	public static void main(String[] args) {
+		DefaultSpecifier defaultSpecifier = new DefaultSpecifier();
+		defaultSpecifier.display();
+	}
+
+}
---
+++ core_basics/src/com/inet/training/defaultpackage/DefaultSpecifier.java
@@ -0,0 +1,9 @@
+package com.inet.training.defaultpackage;
+
+public class DefaultSpecifier {
+	
+	public void display() {
+		System.out.println("Printing nothing");
+	}
+
+}
